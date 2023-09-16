0203c683e2b0e29db4e9d642db7874a61601da88
vladimirtarasov <vptarasov1998@gmail.com>
2023-09-16 11:28:58 +0300

    Add models

---
+++ app/src/main/java/com/vnstudio/cleanarchitecturedemo/Owner.kt
@@ -0,0 +1,23 @@
+package com.vnstudio.cleanarchitecturedemo
+
+
+class Owner() {
+    var login: String? = null
+    var id: Int? = null
+    var nodeId: String? = null
+    var avatarUrl: String? = null
+    var gravatarId: String? = null
+    var url: String? = null
+    var htmlUrl: String? = null
+    var followersUrl: String? = null
+    var followingUrl: String? = null
+    var gistsUrl: String? = null
+    var starredUrl: String? = null
+    var subscriptionsUrl: String? = null
+    var organizationsUrl: String? = null
+    var reposUrl: String? = null
+    var eventsUrl: String? = null
+    var receivedEventsUrl: String? = null
+    var type: String? = null
+    var siteAdmin: Boolean? = null
+}
