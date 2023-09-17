56c712b4d60dff5010d4dab9fc21d55b56bbc0a4
vladimirtarasov <vptarasov1998@gmail.com>
2023-09-17 08:42:02 +0300

    Implement details screen

--- app/src/main/java/com/vnstudio/cleanarchitecturedemo/DetailsActivity.kt
+++ app/src/main/java/com/vnstudio/cleanarchitecturedemo/DetailsActivity.kt
@@ -1,16 +1,51 @@
 package com.vnstudio.cleanarchitecturedemo
 
+import android.graphics.Bitmap
 import android.os.Bundle
-import android.util.Log
+import android.os.Handler
+import android.os.Looper
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.vnstudio.cleanarchitecturedemo.MainActivity.Companion.ERROR
 import com.vnstudio.cleanarchitecturedemo.MainActivity.Companion.FORK
+import com.vnstudio.cleanarchitecturedemo.MainActivity.Companion.SUCCESS_IMAGE_FROM_URL_CONNECTION
 
 class DetailsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val fork = intent.getSerializableExtra(FORK)
-        Log.e("apiTAG", "DetailsActivity onCreate fork $fork")
+        setContentView(R.layout.activity_details)
+        val fork = intent.getSerializableExtra(FORK) as? Fork
+        findViewById<TextView>(R.id.forkName).text = fork?.full_name
+        findViewById<TextView>(R.id.ownerName).text = fork?.owner?.login
+        findViewById<TextView>(R.id.forkDescription).text = fork?.full_name
+        setOwnerAvatar(fork)
+        findViewById<Button>(R.id.backButton).setOnClickListener {
+            onBackPressed()
+        }
     }
+
+    private fun setOwnerAvatar(fork: Fork?) {
+        val ownerAvatar = findViewById<ImageView>(R.id.ownerAvatar)
+        val handler = Handler(Looper.getMainLooper()) { message ->
+            when (message.what) {
+                SUCCESS_IMAGE_FROM_URL_CONNECTION -> {
+                    val imageFromUrl = message.obj as? Bitmap
+                    ownerAvatar.setImageBitmap(imageFromUrl)
+                }
+                ERROR -> {
+                    val errorText = message.obj as String
+                    Toast.makeText(this, errorText, Toast.LENGTH_SHORT).show()
+                }
+            }
+            true
+        }
+        val httpUrlConnector = HttpUrlConnector()
+        httpUrlConnector.getBitmapFromHttpUrlUrl(fork?.owner?.avatar_url, handler)
+    }
+
+
 }
--- app/src/main/java/com/vnstudio/cleanarchitecturedemo/MainActivity.kt
+++ app/src/main/java/com/vnstudio/cleanarchitecturedemo/MainActivity.kt
@@ -8,8 +8,10 @@ import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ListView
+import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,11 +20,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
         val sqLiteDBConnector = SQLiteDBConnector(this)
         val listView = findViewById<ListView>(R.id.listView)
         handler = Handler(Looper.getMainLooper()) { message ->
             when (message.what) {
-                SUCCESS_HTTPS_CONNECTION -> {
+                SUCCESS_HTTP_CONNECTION -> {
                     val responseData = message.obj as String
                     val jsonConverter = JsonConverter()
                     val forks = jsonConverter.getForkList(responseData)
@@ -34,6 +37,7 @@ class MainActivity : AppCompatActivity() {
                     val forkList = message.obj as ArrayList<Fork>
                     val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, forkList.map { it.name })
                     listView.adapter = adapter
+                    progressBar.isVisible = false
                     listView.setOnItemClickListener { _, _, position, _ ->
                         Log.e("apiTAG", "MainActivity SUCCESS_SQLITE_CONNECTION setOnItemClickListener position $position")
                         val intent = Intent(this, DetailsActivity::class.java)
@@ -43,6 +47,7 @@ class MainActivity : AppCompatActivity() {
                     Log.e("apiTAG", "MainActivity SUCCESS_SQLITE_CONNECTION forksNameList.size ${forkList.size}")
                 }
                 ERROR -> {
+                    progressBar.isVisible = false
                     val errorText = message.obj as String
                     Toast.makeText(this, errorText, Toast.LENGTH_SHORT).show()
                 }
@@ -53,14 +58,16 @@ class MainActivity : AppCompatActivity() {
         val startButton = findViewById<Button>(R.id.startButton)
         val httpUrlConnector = HttpUrlConnector()
         startButton.setOnClickListener {
+            progressBar.isVisible = true
             httpUrlConnector.makeHttpUrlConnection(handler)
         }
     }
 
     companion object {
-        const val SUCCESS_HTTPS_CONNECTION = 1
+        const val SUCCESS_HTTP_CONNECTION = 1
         const val SUCCESS_SQLITE_CONNECTION = 2
         const val ERROR = 3
+        const val SUCCESS_IMAGE_FROM_URL_CONNECTION = 4
         const val DATABASE_NAME = "CleanArchitectureDemo"
         const val TABLE_NAME = "forks"
         const val FORK = "fork"
