7cad04a174aa6f0cb9165ef255c5952b0eee33cb
vladimirtarasov <vptarasov1998@gmail.com>
2023-09-19 20:53:53 +0300

    Implement fragments

---
+++ app/src/main/java/com/vnteam/architecturetemplates/ListFragment.kt
@@ -0,0 +1,61 @@
+package com.vnstudio.cleanarchitecturedemo
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.isVisible
+import com.vnstudio.cleanarchitecturedemo.databinding.FragmentListBinding
+
+class ListFragment : Fragment() {
+
+    private var binding: FragmentListBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        binding = FragmentListBinding.inflate(LayoutInflater.from(context))
+        return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding?.progressBar?.isVisible = true
+        val ormLiteSqliteDBConnector = activity?.let { OrmLiteSqliteDBConnector(it) }
+        val httpClientConnector = OkHttpClientConnector()
+        httpClientConnector.makeHttpUrlConnection({ responseData ->
+            val jsonConverter = JsonConverter()
+            responseData?.let {
+                val forks = jsonConverter.getForkList(responseData)
+                ormLiteSqliteDBConnector?.insertDataAsync(jsonConverter.forkListToForkDBList(forks), {
+                    val forkList = ormLiteSqliteDBConnector.getTransformedForks()
+                    val adapter = ForkAdapter(forkList)
+                    adapter.setOnForkClickListener(object : OnForkClickListener {
+                        override fun onForkClick(fork: Fork) {
+                            val detailsFragment = DetailsFragment.newInstance(fork)
+                            parentFragmentManager.beginTransaction().apply {
+                                replace(R.id.fragmentContainer, detailsFragment)
+                                addToBackStack(null)
+                                commit()
+                            }
+                        }
+                    })
+                    binding?.recyclerView?.adapter = adapter
+                    binding?.progressBar?.isVisible = false
+                }, { errorText ->
+                    Toast.makeText(context, errorText, Toast.LENGTH_SHORT).show()
+                })
+            }
+        }, { errorText ->
+            Toast.makeText(context, errorText, Toast.LENGTH_SHORT).show()
+        })
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = ListFragment()
+    }
+}
--- app/src/main/java/com/vnteam/architecturetemplates/MainActivity.kt
+++ app/src/main/java/com/vnteam/architecturetemplates/MainActivity.kt
@@ -1,60 +1,21 @@
-package com.vnteam.architecturetemplates
+package com.vnstudio.cleanarchitecturedemo
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.isVisible
-import butterknife.BindView
-import butterknife.ButterKnife
-import butterknife.OnClick
 
 class MainActivity : AppCompatActivity() {
-
-    @BindView(R.id.progressBar)
-    lateinit var progressBar: ProgressBar
-
-    @BindView(R.id.listView)
-    lateinit var listView: ListView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        ButterKnife.bind(this)
-    }
-
-    @OnClick(R.id.startButton)
-    fun onStartButtonClick() {
-        progressBar.isVisible = true
-        val ormLiteSqliteDBConnector = OrmLiteSqliteDBConnector(this)
-        val httpClientConnector = OkHttpClientConnector()
-        httpClientConnector.makeHttpUrlConnection({ responseData ->
-            val jsonConverter = JsonConverter()
-            responseData?.let {
-                val demoObjects = jsonConverter.getDemoObjectList(responseData)
-                ormLiteSqliteDBConnector.insertDataAsync(jsonConverter.demoObjectListToDemoObjectDBList(demoObjects), {
-                    val demoObjectList = ormLiteSqliteDBConnector.getTransformedDemoObjects()
-                    val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, demoObjectList.map { it.name })
-                    listView.adapter = adapter
-                    progressBar.isVisible = false
-                    listView.setOnItemClickListener { _, _, position, _ ->
-                        Log.e("apiTAG", "MainActivity SUCCESS_SQLITE_CONNECTION setOnItemClickListener position $position")
-                        val intent = Intent(this, DetailsActivity::class.java)
-                        intent.putExtra(DEMO_OBJECT, demoObjectList[position])
-                        startActivity(intent)
-                    }
-                }, { errorText ->
-                    Toast.makeText(this, errorText, Toast.LENGTH_SHORT).show()
-                })
-            }
-        }, { errorText ->
-            Toast.makeText(this, errorText, Toast.LENGTH_SHORT).show()
-        })
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragmentContainer, ListFragment.newInstance())
+            addToBackStack(null)
+            commit()
+        }
     }
 
     companion object {
         const val DATABASE_NAME = "CleanArchitectureDemo"
-        const val DEMO_OBJECT = "demoObject"
     }
 }
