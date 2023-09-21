a465ae7859726753c3795207bfde284f2d3d9fea
vladimirtarasov <vptarasov1998@gmail.com>
2023-09-21 21:33:44 +0300

    Implement presenters

--- app/src/main/java/com/vnstudio/cleanarchitecturedemo/list/ListPresenter.kt
+++ app/src/main/java/com/vnstudio/cleanarchitecturedemo/list/ListPresenter.kt
@@ -1,4 +1,31 @@
 package com.vnstudio.cleanarchitecturedemo.list
 
+import com.vnstudio.cleanarchitecturedemo.JsonConverter
+import com.vnstudio.cleanarchitecturedemo.OkHttpClientConnector
+
 class ListPresenter {
+
+    private var view: ListViewContract? = null
+
+    fun attachView(view: ListViewContract) {
+        this.view = view
+    }
+
+    fun detachView() {
+        this.view = null
+    }
+
+    fun getForksFromApi() {
+        view?.setProgressVisibility(true)
+        val httpClientConnector = OkHttpClientConnector()
+        httpClientConnector.makeHttpUrlConnection({ responseData ->
+            val jsonConverter = JsonConverter()
+            responseData?.let {
+                val forks = jsonConverter.getForkList(responseData)
+                view?.setForks(forks)
+            }
+        }, { errorText ->
+            view?.showError(errorText)
+        })
+    }
 }
---
+++ app/src/main/java/com/vnstudio/cleanarchitecturedemo/list/ListViewContract.kt
@@ -0,0 +1,12 @@
+package com.vnstudio.cleanarchitecturedemo.list
+
+import com.vnstudio.cleanarchitecturedemo.models.Fork
+
+interface ListViewContract {
+
+    fun setProgressVisibility(showProgress: Boolean)
+
+    fun setForks(forks: List<Fork>)
+
+    fun showError(errorMessage: String)
+}
