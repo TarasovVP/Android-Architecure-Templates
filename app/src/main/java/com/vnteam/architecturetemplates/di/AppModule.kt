33c6e3ad656fd765145cb63540423dfe4c7a3e56
vladimirtarasov <vptarasov1998@gmail.com>
2023-09-22 10:34:19 +0300

    Implement volley

--- app/src/main/java/com/vnteam/architecturetemplates/di/AppModule.kt
+++ app/src/main/java/com/vnteam/architecturetemplates/di/AppModule.kt
@@ -1,13 +1,28 @@
 package com.vnstudio.cleanarchitecturedemo.di
 
+import com.vnstudio.cleanarchitecturedemo.AppApplication
+import com.vnstudio.cleanarchitecturedemo.JsonConverter
+import com.vnstudio.cleanarchitecturedemo.ValleyApiConnector
 import com.vnstudio.cleanarchitecturedemo.list.ListPresenter
 import dagger.Module
 import dagger.Provides
 
 @Module
-class AppModule {
+class AppModule(private val application: AppApplication) {
+
+
     @Provides
-    fun provideListPresenter(): ListPresenter {
-        return ListPresenter()
+    fun provideValleyApiConnector(): ValleyApiConnector {
+        return ValleyApiConnector(application)
+    }
+
+    @Provides
+    fun provideJsonConverter(): JsonConverter {
+        return JsonConverter()
+    }
+
+    @Provides
+    fun provideListPresenter(valleyApiConnector: ValleyApiConnector, jsonConverter: JsonConverter): ListPresenter {
+        return ListPresenter(valleyApiConnector, jsonConverter)
     }
 }
