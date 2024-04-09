c65431261f31497bfeb27afd37581da26d2930e0
vladimirtarasov <vptarasov1998@gmail.com>
2024-04-09 11:43:40 +0300

    Implement compose in list screen

--- app/src/main/java/com/vnteam/cleanarchitecturedemo/presentation/list/OnForkClickListener.kt
+++
@@ -1,8 +0,0 @@
-package com.vnteam.cleanarchitecturedemo.presentation.list
-
-import com.vnteam.cleanarchitecturedemo.presentation.uimodels.ForkUI
-
-interface OnForkClickListener {
-
-    fun onForkClick(fork: ForkUI)
-}
