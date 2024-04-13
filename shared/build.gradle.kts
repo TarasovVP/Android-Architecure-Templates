a16de2b12f1edb0ecc56c6b5a5269652a518e7d3
vladimirtarasov <vptarasov1998@gmail.com>
2024-04-13 11:20:29 +0300

    Add voyager

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -25,7 +25,7 @@ kotlin {
 
     sourceSets {
         commonMain.dependencies {
-            //put your multiplatform dependencies here
+            implementation("junit:junit:4.13.2")
         }
         commonTest.dependencies {
             implementation(kotlin("test"))
