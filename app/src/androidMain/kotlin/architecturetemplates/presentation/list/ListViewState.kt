aafbd9cd4ed31136c81305f49a8b9926a3f987cb
vladimirtarasov <vptarasov1998@gmail.com>
2024-04-16 21:10:43 +0300

    Fix ios module logic

--- app/src/androidMain/kotlin/architecturetemplates/presentation/list/ListViewState.kt
+++
@@ -1,7 +0,0 @@
-package architecturetemplates.presentation.list
-
-import com.vnteam.architecturetemplates.presentation.uimodels.ForkUI
-
-data class ListViewState(val forks: List<ForkUI>? = null,
-                         val isLoading: Boolean = false,
-                         val error: String? = null)
---
+++ app/src/iosMain/kotlin/com/vnteam/architecturetemplates/list/ListIntent.kt
@@ -0,0 +1,5 @@
+package com.vnteam.architecturetemplates.list
+
+sealed class ListIntent {
+     class LoadForks : ListIntent()
+}
