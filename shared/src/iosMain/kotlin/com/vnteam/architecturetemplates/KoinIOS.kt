b23d4be49b5593715bc3b952d4395d3576869801
vladimirtarasov <vptarasov1998@gmail.com>
2024-04-19 20:11:02 +0300

    Implement success ios module launching

--- shared/src/iosMain/kotlin/com/vnteam/architecturetemplates/KoinIOS.kt
+++ shared/src/iosMain/kotlin/com/vnteam/architecturetemplates/KoinIOS.kt
@@ -1,7 +1,7 @@
 package com.vnteam.architecturetemplates
-
 import com.vnteam.architecturetemplates.di.appModule
 import org.koin.core.context.startKoin
+
 
 fun doInitKoin() = startKoin {
     modules(appModule, iosModule)
