9b1810dac1adc4ac2ddcf3eecd98c57096110d64
vladimirtarasov <vptarasov1998@gmail.com>
2024-04-24 20:32:31 +0300

    Implement common view models

--- shared/src/iosMain/kotlin/com/vnteam/architecturetemplates/AppNavigation.kt
+++ shared/src/iosMain/kotlin/com/vnteam/architecturetemplates/AppNavigation.kt
@@ -1,11 +1,9 @@
 package com.vnteam.architecturetemplates
 
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
 import com.vnteam.architecturetemplates.presentation.details.DetailsScreen
 import com.vnteam.architecturetemplates.presentation.details.DetailsViewModel
 import com.vnteam.architecturetemplates.presentation.list.ListScreen
@@ -18,16 +16,24 @@ fun AppNavigation() {
 
     NavHost(navController = navController, startDestination = "list") {
 
+        //TODO change forkId implementation
+
         composable("list") {
             val listViewModel = koinInject<ListViewModel>()
-            ListScreen(listViewModel) {
-            val forkId = it.toString()
-            navController.navigate("details/$forkId")
-        } }
-        composable("details/{forkId}", arguments = listOf(navArgument("forkId") { type = NavType.StringType
-            defaultValue = "" })) { backStackEntry ->
-            val forkId = backStackEntry.arguments?.getString("forkId").orEmpty().toLong()
+            ListScreen(listViewModel) { forkId ->
+                navController.currentBackStackEntry?.arguments?.putLong("forkId", forkId)
+                navController.previousBackStackEntry?.arguments?.putLong("forkId", forkId)
+                navController.currentDestination?.arguments?.plus("forkId" to forkId)
+                navController.navigate("details")
+            }
+        }
+        composable("details") { backStackEntry ->
+            val testNavController = navController
+            val forkId = backStackEntry.arguments?.getLong("forkId")
             val detailsViewModel = koinInject<DetailsViewModel>()
+            val current = testNavController.currentBackStackEntry?.arguments?.getLong("forkId", forkId!!)
+            val previous = testNavController.previousBackStackEntry?.arguments?.getLong("forkId", forkId!!)
+            val currentDestination = testNavController.currentDestination?.arguments?.get("forkId")
             DetailsScreen(detailsViewModel, forkId) {
                 navController.popBackStack()
             }
