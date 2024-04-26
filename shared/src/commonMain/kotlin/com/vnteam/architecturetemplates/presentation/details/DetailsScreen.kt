948bdfab07532aea2eda827e210230db58024317
vladimirtarasov <vptarasov1998@gmail.com>
2024-04-26 13:58:36 +0300

    Fix material ripple issue

--- shared/src/commonMain/kotlin/com/vnteam/architecturetemplates/presentation/details/DetailsScreen.kt
+++ shared/src/commonMain/kotlin/com/vnteam/architecturetemplates/presentation/details/DetailsScreen.kt
@@ -10,10 +10,10 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material.Button
-import androidx.compose.material.Card
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Text
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -24,12 +24,11 @@ import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImagePainter
 import coil3.compose.SubcomposeAsyncImage
 import coil3.compose.SubcomposeAsyncImageContent
-import com.vnteam.architecturetemplates.PlatformMessageDisplayer
 import org.koin.compose.koinInject
 
 @Composable
-fun DetailsScreen(viewModel: DetailsViewModel, forkId: Long?, onClick: () -> Unit) {
-    val platformMessageDisplayer: PlatformMessageDisplayer = koinInject()
+fun DetailsScreen(forkId: Long?, onClick: () -> Unit) {
+    val viewModel = koinInject<DetailsViewModel>()
     val viewState = viewModel.state.collectAsState()
 
     LaunchedEffect(forkId) {
@@ -38,11 +37,6 @@ fun DetailsScreen(viewModel: DetailsViewModel, forkId: Long?, onClick: () -> Uni
 
     LaunchedEffect(forkId) {
         viewModel.getForkById(forkId)
-    }
-    LaunchedEffect(viewState.value.error) {
-        viewState.value.error?.let { message ->
-            platformMessageDisplayer.showPopupMessage(message)
-        }
     }
     DetailsContent(viewState.value, onClick)
 }
