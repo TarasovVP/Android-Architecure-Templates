54a2cb56bd1bda81108cb507df4bd2e000df236f
vladimirtarasov <vptarasov1998@gmail.com>
2024-05-05 11:06:39 +0300

    Fix in build gradle

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,10 +1,7 @@
-import java.util.Properties
-
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.composeMultiplatform)
-    alias(libs.plugins.compose.compiler)
     alias(libs.plugins.kotlinSerialization)
     alias(libs.plugins.sqlDelight)
 }
@@ -17,35 +14,49 @@ kotlin {
             }
         }
     }
-    task("testClasses")
-    iosX64()
-    iosArm64()
-    iosSimulatorArm64()
-    js(IR) {
-        useCommonJs()
-        browser()
+
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64(),
+        macosX64(),
+        macosArm64(),
+    ).forEach {
+        it.binaries.framework {
+            freeCompilerArgs += "-Xbinary=bundleId=com.vnteam.architecturetemplates.shared"
+            linkerOpts.add("-lsqlite3")
+            baseName = "shared"
+            isStatic = true
+        }
     }
+    task("testClasses")
     jvm()
     sourceSets {
         commonMain.dependencies {
             api(compose.runtime)
-            implementation(compose.foundation)
-            implementation(compose.runtime)
-            implementation(libs.stately.common)
             implementation(libs.kotlinx.serialization)
             implementation(libs.kotlinx.coroutines.core)
             implementation(libs.androidx.viewmodel.compose)
+            implementation(compose.runtime)
+            implementation(compose.foundation)
+            implementation(compose.material3)
+            implementation(compose.components.resources)
+
             //Ktor
             implementation(libs.ktor.client.core)
             implementation(libs.ktor.client.content.negotiation)
             implementation(libs.ktor.serialization.kotlinx.json)
-            implementation(libs.ktor.client.logging)
             // Koin
             implementation(libs.koin.core)
             implementation(libs.koin.compose)
             //SQLDelight
             implementation(libs.sqldelight.runtime)
             implementation(libs.sqldelight.coroutines.extensions)
+            //Coil
+            implementation(libs.coil.compose)
+            implementation(libs.coil.network.ktor)
+            //Navigation
+            implementation(libs.navigation.compose)
         }
         androidMain.dependencies {
             implementation(libs.ktor.client.android)
@@ -53,11 +64,12 @@ kotlin {
             // Koin
             implementation(libs.koin.android)
             implementation(libs.koin.androidx.compose)
-
-            implementation(libs.androidx.multidex)
         }
         iosMain.dependencies {
             implementation(libs.ktor.client.darwin)
+            implementation(libs.sqldelight.native.driver)
+        }
+        nativeMain.dependencies {
             implementation(libs.sqldelight.native.driver)
         }
         jvmMain.dependencies {
@@ -65,14 +77,6 @@ kotlin {
             implementation(libs.ktor.client.java)
             implementation(libs.kotlinx.coroutines.swing)
             implementation(libs.sqldelight.java.driver)
-            implementation(libs.slf4j)
-        }
-        jsMain.dependencies {
-            implementation(libs.ktor.client.js)
-            implementation(libs.web.worker.driver)
-            implementation(npm("@cashapp/sqldelight-sqljs-worker", "2.0.2"))
-            implementation(npm("sql.js", "1.6.2"))
-            implementation(devNpm("copy-webpack-plugin", "9.1.0"))
         }
     }
 }
@@ -81,44 +85,10 @@ android {
     namespace = "com.vnteam.architecturetemplates.shared"
     compileSdk = libs.versions.compileSdk.get().toInt()
     sourceSets["main"].resources.srcDirs("src/commonMain/resources")
-    defaultConfig {
-        minSdk = libs.versions.minSdk.get().toInt()
-        multiDexEnabled = true
-    }
 }
 
 sqldelight {
-    databases {
-        create("AppDatabase") {
-            packageName.set("com.vnteam.architecturetemplates")
-            generateAsync.set(true)
-            version = 2
-        }
+    database("AppDatabase") {
+        packageName = "com.vnteam.architecturetemplates"
     }
 }
-
-val localProperties = Properties()
-val localPropertiesFile = rootProject.file("local.properties")
-if (localPropertiesFile.exists()) {
-    localProperties.load(localPropertiesFile.inputStream())
-}
-tasks.register("generateBuildConfig") {
-    doLast {
-        val buildConfigFile = file("src/commonMain/kotlin/config/BuildConfig.kt")
-
-        buildConfigFile.parentFile.mkdirs()
-        buildConfigFile.writeText("""
-            package config
-
-            object BuildConfig {
-                val GITHUB_TOKEN: String = "${localProperties.getProperty("github_token", "")}"
-            }
-        """.trimIndent())
-    }
-}
-
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    dependsOn("generateBuildConfig")
-}
-
-
