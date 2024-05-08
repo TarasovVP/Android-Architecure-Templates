717d76e1b15aa75e3ff733c14ae5c92e0925b72a
vladimirtarasov <vptarasov1998@gmail.com>
2024-05-08 12:40:23 +0300

    Add wasm module

--- app/src/jsMain/kotlin/Main.kt
+++ app/src/jsMain/kotlin/Main.kt
@@ -1,14 +1,27 @@
 
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import com.vnteam.architecturetemplates.di.doInitKoin
-import com.vnteam.architecturetemplates.presentation.details.DetailsViewModel
+import com.vnteam.architecturetemplates.presentation.details.DetailsIntent
+import com.vnteam.architecturetemplates.presentation.list.ListIntent
+import com.vnteam.architecturetemplates.presentation.list.ListViewModel
 import org.jetbrains.compose.web.renderComposable
-import org.koin.core.context.GlobalContext.get
+import org.koin.dsl.koinApplication
 
 @OptIn(ExperimentalJsExport::class)
 @JsExport
 fun main() {
     doInitKoin()
+    println("Hello from Compose for Web!")
     renderComposable(rootElementId = "home-section") {
+       /* val listViewModel = koinApplication().koin.get<ListViewModel>()
+        listViewModel.processIntent(ListIntent.LoadForks())
+
+        val forks = listViewModel.state.collectAsState()
+
+        LaunchedEffect(forks.value) {
+            println(forks.value.forks)
+        }*/
 
     }
 }
