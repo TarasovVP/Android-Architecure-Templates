064b9fe86c50b4799711d0681d972548900455df
vladimirtarasov <vptarasov1998@gmail.com>
2024-05-11 11:57:33 +0300

    Implement navigation between screen in web app

--- webApp/src/jsMain/kotlin/Main.kt
+++ webApp/src/jsMain/kotlin/Main.kt
@@ -1,41 +1,14 @@
-
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.mutableStateOf
-import com.vnteam.architecturetemplates.presentation.list.ListIntent
 import di.doInitKoin
+import kotlinx.browser.window
 import org.jetbrains.compose.web.renderComposable
-import org.koin.compose.koinInject
-import presentation.list.ListViewModel
 
 
 fun main() {
     doInitKoin()
     renderComposable(rootElementId = "content-root") {
-
-        val viewModel = koinInject<ListViewModel>()
-        val forks = viewModel.state.collectAsState()
-        val error = mutableStateOf( viewModel.state.value.error )
-
-        LaunchedEffect(forks.value) {
-            error.value = viewModel.state.value.error
+        if (window.location.pathname == "/" || window.location.pathname.isBlank()) {
+            window.history.pushState(null, "", "/list")
         }
-
-        VerticalLayout {
-            StartButton {
-                viewModel.processIntent(ListIntent.LoadForks())
-            }
-            DynamicVerticalList(forks.value.forks?.map { it.fullName } ?: emptyList()) {
-                error.value = "item clicked $it"
-            }
-            if (error.value.isNullOrEmpty().not()) {
-                Toast(error.value.orEmpty()) {
-                    error.value = null
-                }
-            }
-            if (viewModel.state.value.isLoading) {
-                CircularProgress()
-            }
-        }
+        AppContent()
     }
 }
