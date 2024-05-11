023065ca8935592407cda78f3db4b98a534306c4
vladimirtarasov <vptarasov1998@gmail.com>
2024-05-11 11:57:33 +0300

    Implement navigation between screen in web app

---
+++ webApp/src/jsMain/kotlin/App.kt
@@ -0,0 +1,35 @@
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import kotlinx.browser.window
+import org.w3c.dom.events.Event
+
+
+@Composable
+fun AppContent() {
+
+    val currentPath = remember { mutableStateOf(window.location.pathname) }
+
+    DisposableEffect(Unit) {
+        val onPopState: (Event) -> Unit = {
+            currentPath.value = window.location.pathname
+        }
+
+        window.addEventListener("popstate", onPopState)
+
+        onDispose {
+            window.removeEventListener("popstate", onPopState)
+        }
+    }
+    when {
+        currentPath.value.startsWith("/details/") -> {
+            val itemId = currentPath.value.removePrefix("/details/")
+            DetailsScreen(itemId)
+        }
+        currentPath.value == "/list" || currentPath.value.isBlank() || currentPath.value == "/" -> {
+            ListScreen()
+        }
+    }
+}
