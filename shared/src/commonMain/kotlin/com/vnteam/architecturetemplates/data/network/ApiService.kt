28e501af69983f1df76952b5023b420bf26a445d
vladimirtarasov <vptarasov1998@gmail.com>
2024-05-19 19:46:02 +0300

    Add headers to request

--- shared/src/commonMain/kotlin/com/vnteam/architecturetemplates/data/network/ApiService.kt
+++ shared/src/commonMain/kotlin/com/vnteam/architecturetemplates/data/network/ApiService.kt
@@ -1,7 +1,9 @@
 package com.vnteam.architecturetemplates.data.network
 
+import config.BuildConfig.GITHUB_TOKEN
 import io.ktor.client.HttpClient
 import io.ktor.client.request.get
+import io.ktor.client.request.headers
 import io.ktor.client.statement.HttpResponse
 
 
@@ -11,8 +13,15 @@ class ApiService(
 ) {
 
     suspend fun getForks(): HttpResponse {
+
         val httpResponse = try {
-            httpClient.get("${baseUrl}repos/octocat/Spoon-Knife/forks")
+            httpClient.get("${baseUrl}repos/octocat/Spoon-Knife/forks") {
+                headers {
+                    append("Accept", "application/vnd.github+json")
+                    append("Authorization", "Bearer $GITHUB_TOKEN")
+                    append("X-GitHub-Api-Version", "2022-11-28")
+                }
+            }
         } catch (e: Exception) {
             e.printStackTrace()
             throw e
