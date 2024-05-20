e5168fbfd8aec1e1f29ea6e3fa75ba1a44dafda7
vladimirtarasov <vptarasov1998@gmail.com>
2024-05-20 16:35:23 +0300

    Update js sqldelight

--- shared/src/jsMain/kotlin/com/vnteam/architecturetemplates/data/database/DatabaseDriverFactory.test.kt
+++ shared/src/jsMain/kotlin/com/vnteam/architecturetemplates/data/database/DatabaseDriverFactory.test.kt
@@ -1,14 +1,17 @@
 package com.vnteam.architecturetemplates.data.database
 
 import app.cash.sqldelight.db.SqlDriver
-import app.cash.sqldelight.driver.sqljs.initSqlDriver
-import com.vnteam.architecturetemplates.AppDatabase
-import kotlinx.coroutines.await
+import app.cash.sqldelight.driver.worker.WebWorkerDriver
+import org.w3c.dom.Worker
 
 
 @Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
 actual class DatabaseDriverFactory {
     actual suspend fun createDriver(): SqlDriver {
-        return initSqlDriver(AppDatabase.Schema).await()
+        return WebWorkerDriver(
+            Worker(
+                js("""new URL("@cashapp/sqldelight-sqljs-worker/sqljs.worker.js", import.meta.url)""")
+            )
+        )
     }
 }
