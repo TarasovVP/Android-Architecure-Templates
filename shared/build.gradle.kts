8f966c17a5f94a0d21ae59b733d8b2f7c887f662
vladimirtarasov <vptarasov1998@gmail.com>
2024-05-21 20:01:30 +0300

    Add logging to network requests

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -38,6 +38,7 @@ kotlin {
             implementation(libs.ktor.client.core)
             implementation(libs.ktor.client.content.negotiation)
             implementation(libs.ktor.serialization.kotlinx.json)
+            implementation(libs.ktor.client.logging)
             // Koin
             implementation(libs.koin.core)
             implementation(libs.koin.compose)
