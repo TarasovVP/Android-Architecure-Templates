b56b7aa6beef60b4633f73f19b04aab3b3391cc0
vladimirtarasov <vptarasov1998@gmail.com>
2024-05-24 21:45:54 +0300

    Implement object deletion in db

--- shared/src/commonMain/kotlin/com/vnteam/architecturetemplates/presentation/intents/ListIntent.kt
+++ shared/src/commonMain/kotlin/com/vnteam/architecturetemplates/presentation/intents/ListIntent.kt
@@ -3,5 +3,5 @@ package com.vnteam.architecturetemplates.presentation.intents
 sealed class ListIntent {
      class ClearForks : ListIntent()
      class LoadForks : ListIntent()
-     class DeleteFork : ListIntent()
+     class DeleteFork(val id: Long) : ListIntent()
 }
