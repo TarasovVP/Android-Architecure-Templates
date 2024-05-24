0bfcb9efcb23060bbb35a03de415a599caec3847
vladimirtarasov <vptarasov1998@gmail.com>
2024-05-24 21:45:54 +0300

    Implement object deletion in db

--- shared/src/commonMain/kotlin/com/vnteam/architecturetemplates/presentation/viewmodels/ListViewModel.kt
+++ shared/src/commonMain/kotlin/com/vnteam/architecturetemplates/presentation/viewmodels/ListViewModel.kt
@@ -2,33 +2,37 @@ package com.vnteam.architecturetemplates.presentation.viewmodels
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.vnteam.architecturetemplates.PlatformCoroutineDispatcher
 import com.vnteam.architecturetemplates.domain.models.Fork
 import com.vnteam.architecturetemplates.presentation.mappers.ForkUIMapper
 import com.vnteam.architecturetemplates.domain.usecase.ForkUseCase
 import com.vnteam.architecturetemplates.presentation.intents.ListIntent
+import com.vnteam.architecturetemplates.presentation.resources.getStringResources
 import com.vnteam.architecturetemplates.presentation.states.ListViewState
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 class ListViewModel(
     private val forkUseCase: ForkUseCase,
-    private val forkUIMapper: ForkUIMapper,
-    private val platformCoroutineDispatcher: PlatformCoroutineDispatcher
+    private val forkUIMapper: ForkUIMapper
 ) : ViewModel() {
 
     private val _state = MutableStateFlow(ListViewState())
     val state: StateFlow<ListViewState> = _state.asStateFlow()
 
+    private val exceptionHandler = CoroutineExceptionHandler { _, exception ->
+        _state.value = state.value.copy(isLoading = false, error = exception.message)
+    }
+
     fun processIntent(intent: ListIntent) {
         when (intent) {
             is ListIntent.ClearForks -> clearForks()
             is ListIntent.LoadForks -> getForksFromApi()
-            is ListIntent.DeleteFork -> getForksFromApi()
+            is ListIntent.DeleteFork -> deleteForkById(intent.id)
         }
     }
 
@@ -39,19 +43,15 @@ class ListViewModel(
     }
 
     private fun getForksFromApi() {
-        viewModelScope.launch(CoroutineExceptionHandler { _, exception ->
-            _state.value = state.value.copy(isLoading = false, error = exception.message)
-        }) {
-            _state.value = state.value.copy(isLoading = true)
+        _state.value = state.value.copy(isLoading = true)
+        viewModelScope.launch(exceptionHandler) {
             val forks = forkUseCase.getForksFromApi()
             insertForksToDB(forks)
         }
     }
 
     private fun insertForksToDB(forks: Flow<List<Fork>?>) {
-        viewModelScope.launch(CoroutineExceptionHandler { _, exception ->
-            _state.value = state.value.copy(isLoading = false, error = exception.message)
-        }) {
+        viewModelScope.launch(exceptionHandler) {
             forks.collect {
                 it ?: return@collect
                 forkUseCase.insertForksToDB(it)
@@ -61,9 +61,7 @@ class ListViewModel(
     }
 
     private fun getForksFromDB() {
-        viewModelScope.launch(CoroutineExceptionHandler { _, exception ->
-            _state.value = state.value.copy(isLoading = false, error = exception.message)
-        }) {
+        viewModelScope.launch(exceptionHandler) {
             forkUseCase.getForksFromDB().collect {
                 val forks = forkUIMapper.mapToImplModelList(it)
                 _state.value = state.value.copy(forks = forks, isLoading = false)
@@ -71,11 +69,12 @@ class ListViewModel(
         }
     }
 
-    private fun deleteFork() {
+    private fun deleteForkById(forkId: Long) {
+        _state.value = state.value.copy(isLoading = true)
         viewModelScope.launch(exceptionHandler) {
-            forkUseCase.getForksFromDB().collect {
-                val forks = forkUIMapper.mapToImplModelList(it)
-                _state.value = state.value.copy(forks = forks, isLoading = false)
+            forkUseCase.deleteForkById(forkId).collect {
+                getForksFromDB()
+                _state.value = state.value.copy(success = "Success deleted", isLoading = false)
             }
         }
     }
