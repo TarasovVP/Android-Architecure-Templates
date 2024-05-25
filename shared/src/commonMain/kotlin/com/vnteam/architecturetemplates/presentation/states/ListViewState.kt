fefcd4d3d4dcb58fb35d93891b4997057d811361
vladimirtarasov <vptarasov1998@gmail.com>
2024-05-25 21:37:48 +0300

    Add snackbar

--- shared/src/commonMain/kotlin/com/vnteam/architecturetemplates/presentation/states/ListViewState.kt
+++ shared/src/commonMain/kotlin/com/vnteam/architecturetemplates/presentation/states/ListViewState.kt
@@ -4,5 +4,4 @@ import com.vnteam.architecturetemplates.presentation.uimodels.ForkUI
 
 data class ListViewState(val forks: List<ForkUI>? = null,
                          val isLoading: Boolean = false,
-                         val error: String? = null,
-                         val success: String? = null)
+                         var infoMessage: InfoMessageState? = null)
