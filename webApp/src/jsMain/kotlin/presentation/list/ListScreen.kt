a12c1a04dcf7fb0e9dfa3d03849a21d2179538a6
vladimirtarasov <vptarasov1998@gmail.com>
2024-05-25 21:37:48 +0300

    Add snackbar

--- webApp/src/jsMain/kotlin/presentation/list/ListScreen.kt
+++ webApp/src/jsMain/kotlin/presentation/list/ListScreen.kt
@@ -6,16 +6,19 @@ import androidx.compose.material3.SnackbarVisuals
 import components.BaseButton
 import components.CircularProgress
 import components.DynamicVerticalList
+import components.Toast
 import components.VerticalLayout
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.graphics.Color
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.vnteam.architecturetemplates.presentation.intents.ListIntent
-import com.vnteam.architecturetemplates.presentation.resources.LocalStringResources
 import com.vnteam.architecturetemplates.presentation.viewmodels.ListViewModel
 import kotlinx.browser.window
 import org.w3c.dom.events.Event
+import com.vnteam.architecturetemplates.presentation.resources.getStringResources
+import com.vnteam.architecturetemplates.presentation.viewmodels.viewModel
 
 
 @Composable
@@ -25,7 +28,7 @@ fun ListScreen() {
     val forks = viewModel.state.collectAsState()
 
     VerticalLayout {
-        BaseButton(LocalStringResources.current.START) {
+        BaseButton(getStringResources().START) {
             viewModel.processIntent(ListIntent.LoadForks())
         }
         DynamicVerticalList(forks.value.forks?.map { it.fullName } ?: emptyList()) { itemName ->
@@ -36,7 +39,7 @@ fun ListScreen() {
         if (viewModel.state.value.isLoading) {
             CircularProgress()
         }
-        viewModel.state.value.infoMessage.value.takeIf { it != null }?.let {
+        viewModel.state.value.infoMessage.takeIf { it != null }?.let {
             Snackbar(snackbarData = object : SnackbarData {
                 override val visuals: SnackbarVisuals
                     get() = object : SnackbarVisuals {
