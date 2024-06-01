bf804ed713897872612067aa694ad11dfee49a6d
vladimirtarasov <vptarasov1998@gmail.com>
2024-06-01 11:27:58 +0300

    Minor fixes

--- composeApp/src/iosMain/kotlin/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/MainViewController.kt
@@ -1,15 +1,15 @@
 import androidx.compose.ui.window.ComposeUIViewController
 import platform.UIKit.UIViewController
-import presentation.AppNavigation
 import theme.AppTheme
 import com.vnteam.architecturetemplates.presentation.viewmodels.withViewModelStoreOwner
+import presentation.App
 
 fun MainViewController(): UIViewController {
 
     return ComposeUIViewController {
         withViewModelStoreOwner {
             AppTheme {
-                AppNavigation()
+                App()
             }
         }
     }
--- composeApp/src/jsMain/kotlin/main.kt
+++ composeApp/src/jsMain/kotlin/main.kt
@@ -2,14 +2,14 @@ import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.CanvasBasedWindow
 import com.vnteam.architecturetemplates.di.doInitKoin
 import org.jetbrains.skiko.wasm.onWasmReady
-import presentation.AppNavigation
+import presentation.App
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
     doInitKoin()
     onWasmReady {
         CanvasBasedWindow("Architecture Templates") {
-            AppNavigation()
+            App()
         }
     }
 }
