e64b7dac48bf2eecbbd2770da7ae2d873dcbd365
TarasovVP <vptarasov1998@gmail.com>
2024-07-19 21:32:54 +0300

    Add splash screen

--- composeApp/src/commonMain/kotlin/presentation/components/Common.kt
+++ composeApp/src/commonMain/kotlin/presentation/components/Common.kt
@@ -1,34 +1,38 @@
 package presentation.components
 
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.input.TextFieldValue
@@ -43,33 +47,66 @@ import androidx.compose.material3.SnackbarVisuals
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.input.ImeAction
 import kotlinx.coroutines.delay
 import coil3.compose.AsyncImagePainter
 import coil3.compose.SubcomposeAsyncImage
 import coil3.compose.SubcomposeAsyncImageContent
-import com.vnteam.architecturetemplates.presentation.resources.DrawableResources
+import com.vnteam.architecturetemplates.Res
+import com.vnteam.architecturetemplates.android_architecture_template
+import com.vnteam.architecturetemplates.ic_avatar_default
 import com.vnteam.architecturetemplates.presentation.resources.LocalLargePadding
 import com.vnteam.architecturetemplates.presentation.resources.LocalMediumPadding
 import com.vnteam.architecturetemplates.presentation.resources.LocalSmallPadding
 import com.vnteam.architecturetemplates.presentation.resources.LocalStringResources
 import com.vnteam.architecturetemplates.presentation.states.InfoMessageState
-import org.jetbrains.compose.resources.DrawableResource
-import org.jetbrains.compose.resources.InternalResourceApi
-import org.jetbrains.compose.resources.ResourceItem
 import org.jetbrains.compose.resources.painterResource
 import theme.Neutral400
 import theme.Neutral700
 import theme.Primary400
 import theme.Primary500
 
-@OptIn(InternalResourceApi::class)
 @Composable
-fun painterRes(resId: String): Painter {
-    //TODO use painterResource("drawable/${resId}.xml")
-    return painterResource(DrawableResource("", setOf(ResourceItem(setOf(),"drawable/${resId}.xml", 100, 100))))
+fun SplashScreen() {
+    val screenWidth = remember { mutableStateOf(0.dp) }
+    val minSize = screenWidth.value * 0.2f
+    val maxSize = screenWidth.value * 0.7f
+
+    val infiniteTransition = rememberInfiniteTransition()
+    val size = infiniteTransition.animateFloat(
+        initialValue = minSize.value,
+        targetValue = maxSize.value,
+        animationSpec = infiniteRepeatable(
+            animation = tween(durationMillis = 700, easing = LinearEasing),
+            repeatMode = RepeatMode.Reverse
+        )
+    )
+
+    val localDensity = LocalDensity.current
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .onSizeChanged { size ->
+                with(localDensity) {
+                    screenWidth.value = size.width.toDp()
+                }
+            },
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            painter = painterResource(Res.drawable.android_architecture_template),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.size(size.value.dp).clip(RoundedCornerShape(16.dp))
+        )
+    }
 }
+
 
 @Composable
 fun HeaderText(
@@ -127,7 +164,7 @@ fun AvatarImage(avatarUrl: String, avatarSize: Dp) {
     ) {
         when (painter.state) {
             is AsyncImagePainter.State.Loading -> CircularProgressIndicator()
-            is AsyncImagePainter.State.Error -> Image(painter = painterRes(DrawableResources.IC_PERSON), contentDescription = null)
+            is AsyncImagePainter.State.Error -> Image(painter = painterResource(Res.drawable.ic_avatar_default), contentDescription = null)
             else -> SubcomposeAsyncImageContent()
         }
     }
