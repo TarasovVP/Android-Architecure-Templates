149ac4df9661012c5477529b3d0ff6b21ae0209e
Volodymyr Tarasov <34417782+TarasovVP@users.noreply.github.com>
2024-09-25 21:05:32 +0300

    Rename demo object

--- app/src/main/java/com/vnteam/architecturetemplates/presentation/mappers/ForkUIMapper.kt
+++
@@ -1,7 +0,0 @@
-package com.vnteam.architecturetemplates.presentation.mappers
-
-import com.vnteam.architecturetemplates.domain.mappers.BaseMapper
-import com.vnteam.architecturetemplates.domain.models.Fork
-import com.vnteam.architecturetemplates.presentation.uimodels.ForkUI
-
-interface ForkUIMapper : BaseMapper<Fork, ForkUI>
