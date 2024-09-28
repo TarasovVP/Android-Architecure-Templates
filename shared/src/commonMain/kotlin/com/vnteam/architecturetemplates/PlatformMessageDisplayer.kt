829a1800ecba70ff3938e31b6ac20844b4b33aa6
Volodymyr Tarasov <34417782+TarasovVP@users.noreply.github.com>
2024-09-28 12:37:42 +0300

    Fix merge conflicts

--- shared/src/commonMain/kotlin/com/vnteam/architecturetemplates/PlatformMessageDisplayer.kt
+++
@@ -1,8 +0,0 @@
-package com.vnteam.architecturetemplates
-
-@Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
-expect class PlatformMessageDisplayer {
-
-    fun showPopupMessage(message: String)
-
-}
