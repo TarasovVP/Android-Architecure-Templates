d8e2d5934341131ae2a542131869f68fb50f7f81
Volodymyr Tarasov <34417782+TarasovVP@users.noreply.github.com>
2024-09-28 12:37:42 +0300

    Fix merge conflicts

--- shared/src/iosMain/kotlin/com/vnteam/architecturetemplates/Platform.ios.kt
+++
@@ -1,9 +0,0 @@
-package com.vnteam.architecturetemplates
-
-import platform.UIKit.UIDevice
-
-class IOSPlatform: Platform {
-    override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
-}
-
-actual fun getPlatform(): Platform = IOSPlatform()
