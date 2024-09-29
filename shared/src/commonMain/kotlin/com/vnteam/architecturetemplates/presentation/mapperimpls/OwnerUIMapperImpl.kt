79e3023697181dddb52375eefbe18664851bc9d3
Volodymyr Tarasov <34417782+TarasovVP@users.noreply.github.com>
2024-09-29 08:21:20 +0300

    Fix merge conflicts

--- shared/src/commonMain/kotlin/com/vnteam/architecturetemplates/presentation/mapperimpls/OwnerUIMapperImpl.kt
+++ shared/src/commonMain/kotlin/com/vnteam/architecturetemplates/presentation/mapperimpls/OwnerUIMapperImpl.kt
@@ -1,7 +1,6 @@
 package com.vnteam.architecturetemplates.presentation.mapperimpls
 
 import presentation.mappers.OwnerUIMapper
-import presentation.mappers.OwnerUIMapper
 import com.vnteam.architecturetemplates.domain.models.Owner
 import com.vnteam.architecturetemplates.presentation.uimodels.OwnerUI
 
