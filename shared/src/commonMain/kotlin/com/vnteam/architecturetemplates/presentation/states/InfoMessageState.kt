9c2151681258f1bc322cc3570404bddd3e715e3c
Volodymyr Tarasov <34417782+TarasovVP@users.noreply.github.com>
2024-10-01 20:09:42 +0300

    Fix git ignore issues

--- shared/src/commonMain/kotlin/com/vnteam/architecturetemplates/presentation/states/InfoMessageState.kt
+++
@@ -1,4 +0,0 @@
-package com.vnteam.architecturetemplates.presentation.states
-
-data class InfoMessageState(val message: String = "",
-                            val isError: Boolean = false)
--- shared/src/commonMain/kotlin/com/vnteam/architecturetemplates/resources/DrawableResources.kt
+++
@@ -1,7 +0,0 @@
-package com.vnteam.architecturetemplates.resources
-
-object DrawableResources {
-    const val IC_PERSON = "ic_person"
-    const val EMPTY_STATE = "empty_state"
-
-}
