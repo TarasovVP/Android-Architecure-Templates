960f7afb1b282acc00abb69ae78e38c2426a514c
Volodymyr Tarasov <34417782+TarasovVP@users.noreply.github.com>
2024-10-07 20:02:43 +0300

    Change docker file

--- server/src/main/kotlin/com/vnteam/architecturetemplates/db/DatabaseDriverFactory.kt
+++ server/src/main/kotlin/com/vnteam/architecturetemplates/db/DatabaseDriverFactory.kt
@@ -12,11 +12,10 @@ import com.zaxxer.hikari.HikariDataSource
 class DatabaseDriverFactory {
     fun createDriver(): SqlDriver {
         val config = HikariConfig().apply {
-            jdbcUrl = JDBC_URL
-            driverClassName = DRIVER_CLASS_NAME
-
-            username = DB_USER_NAME
-            password = DB_USER_PASSWORD
+            jdbcUrl = System.getenv("JDBC_URL") ?: JDBC_URL
+            driverClassName = System.getenv("DRIVER_CLASS_NAME") ?: DRIVER_CLASS_NAME
+            username = System.getenv("DB_USER_NAME") ?: DB_USER_NAME
+            password = System.getenv("DB_USER_PASSWORD") ?: DB_USER_PASSWORD
 
             maximumPoolSize = 5
             minimumIdle = 2
