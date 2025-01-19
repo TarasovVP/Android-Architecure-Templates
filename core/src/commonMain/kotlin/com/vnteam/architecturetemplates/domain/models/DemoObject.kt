e1cc804407db8893ebd0e9e3aa8c426675aa922a
TarasovVP <vptarasov1998@gmail.com>
2025-01-19 11:15:22 +0200

    Fix imports

--- core/src/commonMain/kotlin/com/vnteam/architecturetemplates/domain/models/DemoObject.kt
+++
@@ -1,9 +0,0 @@
-package com.vnteam.architecturetemplates.domain.models
-
-data class DemoObject(
-    var demoObjectId: String? = null,
-    var name: String? = null,
-    var owner: Owner? = null,
-    var htmlUrl: String? = null,
-    var description: String? = null,
-)
--- core/src/commonMain/kotlin/com/vnteam/architecturetemplates/domain/models/Owner.kt
+++
@@ -1,8 +0,0 @@
-package com.vnteam.architecturetemplates.domain.models
-
-data class Owner(
-    var login: String? = null,
-    var ownerId: String? = null,
-    var avatarUrl: String? = null,
-    var url: String? = null
-)
