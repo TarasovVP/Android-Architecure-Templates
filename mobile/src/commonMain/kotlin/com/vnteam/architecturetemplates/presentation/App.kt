02aae7d3bf7095fe13cc242d5d596b01980cdaf3
TarasovVP <vptarasov1998@gmail.com>
2025-04-13 10:38:00 +0300

    Add pre-commit hook

--- mobile/src/commonMain/kotlin/com/vnteam/architecturetemplates/presentation/App.kt
+++ mobile/src/commonMain/kotlin/com/vnteam/architecturetemplates/presentation/App.kt
@@ -50,6 +50,7 @@ fun App(appViewModel: AppViewModel) {
     val language = appViewModel.language.collectAsState()
     val screenState = remember { mutableStateOf(ScreenState()) }
     "sdsd"
+
     CompositionLocalProvider(LocalStringResources provides getStringResourcesByLocale(language.value.orEmpty())) {
         isDarkTheme.value?.let {
             AppTheme(it) {
