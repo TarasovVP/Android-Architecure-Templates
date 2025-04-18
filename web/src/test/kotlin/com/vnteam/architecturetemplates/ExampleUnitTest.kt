45368aa628d47e6566a8d9aec64f77ce0efad447
TarasovVP <vptarasov1998@gmail.com>
2025-04-18 20:54:56 +0300

    Fix detect issues

--- web/src/test/kotlin/com/vnteam/architecturetemplates/ExampleUnitTest.kt
+++ web/src/test/kotlin/com/vnteam/architecturetemplates/ExampleUnitTest.kt
@@ -2,8 +2,6 @@ package com.vnteam.architecturetemplates
 
 import org.junit.Test
 
-import org.junit.Assert.*
-
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
