59bb8edb000a776ae0f883313cd152c0df29514a
TarasovVP <vptarasov1998@gmail.com>
2025-06-02 20:40:38 +0300

    Improve MappersBenchmark

--- shared/src/commonBenchmark/kotlin/benchmarks/MappersBenchmark.kt
+++ shared/src/commonBenchmark/kotlin/benchmarks/MappersBenchmark.kt
@@ -1,13 +1,12 @@
 package benchmarks
 
 import com.vnteam.architecturetemplates.data.mapperimpls.DemoObjectResponseMapperImpl
-import com.vnteam.architecturetemplates.domain.mappers.OwnerUIMapper
+import com.vnteam.architecturetemplates.data.mapperimpls.OwnerResponseMapperImpl
+import com.vnteam.architecturetemplates.data.network.responses.DemoObjectResponse
 import com.vnteam.architecturetemplates.domain.models.DemoObject
-import com.vnteam.architecturetemplates.domain.models.Owner
-import com.vnteam.architecturetemplates.domain.responses.DemoObjectResponse
 import com.vnteam.architecturetemplates.presentation.mapperimpls.DemoObjectUIMapperImpl
+import com.vnteam.architecturetemplates.presentation.mapperimpls.OwnerUIMapperImpl
 import com.vnteam.architecturetemplates.presentation.uimodels.DemoObjectUI
-import com.vnteam.architecturetemplates.presentation.uimodels.OwnerUI
 import kotlinx.benchmark.Benchmark
 import kotlinx.benchmark.BenchmarkMode
 import kotlinx.benchmark.BenchmarkTimeUnit
@@ -19,7 +18,7 @@ import kotlinx.benchmark.State
 
 @State(Scope.Benchmark)
 @BenchmarkMode(Mode.AverageTime)
-@OutputTimeUnit(BenchmarkTimeUnit.MICROSECONDS) // Используем микросекунды для мапперов
+@OutputTimeUnit(BenchmarkTimeUnit.MICROSECONDS)
 class MappersBenchmark {
     private lateinit var testObject: DemoObject
     private lateinit var testObjectList: List<DemoObject>
@@ -29,74 +28,42 @@ class MappersBenchmark {
 
     @Setup
     fun setup() {
-        responseMapper = DemoObjectResponseMapperImpl(OwnerResponseMapperStub())
-        uiMapper = DemoObjectUIMapperImpl(OwnerUIMapperStub())
+        responseMapper = DemoObjectResponseMapperImpl(OwnerResponseMapperImpl())
+        uiMapper = DemoObjectUIMapperImpl(OwnerUIMapperImpl())
 
         testObject = MapperTestData.createTestDemoObject()
         testObjectList = MapperTestData.createTestDemoObjectList(100)
     }
 
     @Benchmark
-    fun singleObjectTransformationChain(): DemoObjectUI = runBlocking {
+    fun singleObjectTransformationChain(): DemoObjectUI {
         val response = responseMapper.mapToImplModel(testObject)
-        uiMapper.mapToImplModel(responseMapper.mapFromImplModel(response))
+        return uiMapper.mapToImplModel(responseMapper.mapFromImplModel(response))
     }
 
     @Benchmark
-    fun listTransformationChain(): List<DemoObjectUI> = runBlocking {
+    fun listTransformationChain(): List<DemoObjectUI> {
         val responses = responseMapper.mapToImplModelList(testObjectList)
-        uiMapper.mapToImplModelList(responseMapper.mapFromImplModelList(responses))
+        return uiMapper.mapToImplModelList(responseMapper.mapFromImplModelList(responses))
     }
 
     @Benchmark
-    fun responseMapperSingle(): DemoObjectResponse = runBlocking {
-        responseMapper.mapToImplModel(testObject)
+    fun responseMapperSingle(): DemoObjectResponse {
+        return responseMapper.mapToImplModel(testObject)
     }
 
     @Benchmark
-    fun responseMapperList(): List<DemoObjectResponse> = runBlocking {
-        responseMapper.mapToImplModelList(testObjectList)
+    fun responseMapperList(): List<DemoObjectResponse> {
+        return responseMapper.mapToImplModelList(testObjectList)
     }
 
     @Benchmark
-    fun uiMapperSingle(): DemoObjectUI = runBlocking {
-        uiMapper.mapToImplModel(testObject)
+    fun uiMapperSingle(): DemoObjectUI {
+        return uiMapper.mapToImplModel(testObject)
     }
 
     @Benchmark
-    fun uiMapperList(): List<DemoObjectUI> = runBlocking {
-        uiMapper.mapToImplModelList(testObjectList)
+    fun uiMapperList(): List<DemoObjectUI> {
+        return uiMapper.mapToImplModelList(testObjectList)
     }
-}
-
-private class OwnerResponseMapperStub : OwnerResponseMapper {
-    override fun mapToImplModel(from: Owner): OwnerResponse = OwnerResponse(
-        ownerId = from.ownerId,
-        login = from.login,
-        avatarUrl = from.avatarUrl,
-        htmlUrl = from.htmlUrl
-    )
-
-    override fun mapFromImplModel(to: OwnerResponse): Owner = Owner(
-        ownerId = to.ownerId,
-        login = to.login,
-        avatarUrl = to.avatarUrl,
-        htmlUrl = to.htmlUrl
-    )
-}
-
-private class OwnerUIMapperStub : OwnerUIMapper {
-    override fun mapToImplModel(from: Owner): OwnerUI = OwnerUI(
-        ownerId = from.ownerId,
-        login = from.login,
-        avatarUrl = from.avatarUrl,
-        htmlUrl = from.htmlUrl
-    )
-
-    override fun mapFromImplModel(to: OwnerUI): Owner = Owner(
-        ownerId = to.ownerId,
-        login = to.login,
-        avatarUrl = to.avatarUrl,
-        htmlUrl = to.htmlUrl
-    )
 }
