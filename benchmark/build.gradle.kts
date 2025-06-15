86663abeb8055bc5dbcba53eb0d1a04959d18863
TarasovVP <vptarasov1998@gmail.com>
2025-06-15 09:40:32 +0300

    Add macro benchmark module

---
+++ benchmark/build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    id(libs.plugins.androidTest.get().pluginId)
+    kotlin("android")
+}
+
+android {
+    namespace = "com.vnteam.benchmark"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 24
+        targetSdk = 35
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        create("benchmark") {
+            isDebuggable = true
+            signingConfig = getByName("debug").signingConfig
+            matchingFallbacks += listOf("release")
+        }
+    }
+
+    targetProjectPath = ":mobile"
+    experimentalProperties["android.experimental.self-instrumenting"] = true
+}
+
+dependencies {
+    implementation(libs.androidx.junit)
+    implementation(libs.androidx.espresso.core)
+    implementation(libs.androidx.uiautomator)
+    implementation(libs.androidx.benchmark.macro.junit4)
+}
+
+androidComponents {
+    beforeVariants(selector().all()) {
+        it.enable = it.buildType == "benchmark"
+    }
+}
