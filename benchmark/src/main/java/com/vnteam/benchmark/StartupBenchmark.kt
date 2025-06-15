f78455aa040c0e5cb8fa9d20e6ed4da8e5873342
TarasovVP <vptarasov1998@gmail.com>
2025-06-15 10:35:52 +0300

    Minor improve

--- benchmark/src/main/java/com/vnteam/benchmark/StartupBenchmark.kt
+++ benchmark/src/main/java/com/vnteam/benchmark/StartupBenchmark.kt
@@ -9,7 +9,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class ExampleStartupBenchmark {
+class StartupBenchmark {
     @get:Rule
     val benchmarkRule = MacrobenchmarkRule()
 
