4062f86958aef19b08d5929d7c6be8698bb7a825
TarasovVP <vptarasov1998@gmail.com>
2025-09-22 20:06:26 +0200

    Improve init koin with koin lambda

--- mobile/src/androidMain/kotlin/com/vnteam/architecturetemplates/AppApplication.kt
+++ mobile/src/androidMain/kotlin/com/vnteam/architecturetemplates/AppApplication.kt
@@ -2,17 +2,10 @@ package com.vnteam.architecturetemplates
 
 import android.app.Application
 import com.vnteam.architecturetemplates.di.initKoin
-import org.koin.android.ext.koin.androidContext
-import org.koin.android.ext.koin.androidLogger
 
 class AppApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        initKoin { koinApplication ->
-            koinApplication.apply {
-                androidLogger()
-                androidContext(this@AppApplication)
-            }
-        }
+        initKoin(this)
     }
 }
--- shared/src/androidMain/kotlin/com/vnteam/architecturetemplates/di/Koin.android.kt
+++ shared/src/androidMain/kotlin/com/vnteam/architecturetemplates/di/Koin.android.kt
@@ -1,5 +1,15 @@
 package com.vnteam.architecturetemplates.di
 
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
 import org.koin.core.module.Module
 
 actual fun platformModule(): Module = androidModule
+
+fun initKoin(application: Application) {
+    initKoin { koinApplication ->
+        koinApplication.apply {
+            androidContext(application)
+        }
+    }
+}
